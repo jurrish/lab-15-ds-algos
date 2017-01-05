d2feeb556f97a1d087ee441bc117b1cf4a743fc4
Frank Iriarte <jurrish@gmail.com>
2017-01-04 19:59:06 -0800

    added removeTail and removeHead to dll proto

--- lab-15-JR/double-linked-list.js
+++ lab-15-JR/double-linked-list.js
@@ -36,5 +36,23 @@ DoublyLinkedList.prototype.append = function(val) {
   return node.val;
 };
 
+DoublyLinkedList.prototype.removeHead = function() {
+  if(!this.head) return;
+  let prevNode = this.head;
+  this.head = prevNode.next;
+  prevNode.next = null;
+  this.head.prev = null;
+  return prevNode.val;
+};
+
+DoublyLinkedList.prototype.removeTail = function() {
+  if(!this.tail) return;
+  let prevNode = this.tail;
+  this.tail = prevNode.prev;
+  this.tail.next = null;
+  prevNode.prev = null;
+  return prevNode.val;
+};
+
 //HEAD[val, prev, next] <=> [val, prev, next] <=> [val, prev, next](TAIL)
 // prepend()                                              append()
